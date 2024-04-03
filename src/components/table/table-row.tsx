0df544b5b33c312768b4782a658c2c8e7f68c5aa
Jonathan Teixeira <jonathancateixeira@outlook.com>
2024-04-03 14:47:01 -0300

    feat: :sparkles: Segunda aula NLW - Unite

---
+++ src/components/table/table-row.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from "react";
+import { twMerge } from 'tailwind-merge'
+
+interface TableRowProps extends ComponentProps<'tr'> {
+
+}
+
+export function TableRow(props: TableRowProps) {
+    return (
+        <tr {...props} className="border-b border-white/10 hover:bg-white/5"></tr>
+    )
+}
