6b96fddc9911f16605210b61c0472ffeb45521f9
Jonathan Teixeira <jonathancateixeira@outlook.com>
2024-04-05 15:46:46 -0300

    feat: :sparkles: Implementa rotas para aba de participantes e eventos

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,24 @@
-import { AttendeeList } from "./components/attendee-list";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AttendeeList } from "./pages/attendee-list";
 import { Header } from "./components/header";
+import { Events } from "./pages/events";
+import { NotFound } from "./pages/404";
 
 export function App() {
     return (
         <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
             <Header />
-            <AttendeeList />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/eventos" Component={Events}></Route>
+                    <Route path="/participantes" Component={AttendeeList}></Route>
+                    <Route path="*" Component={NotFound}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
+
+
+
+
